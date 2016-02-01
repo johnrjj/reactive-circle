589f216eb55132ed36a070f5c0c4c673160bb552
John J <johnrjj@gmail.com>
2016-02-01 11:19:59 -0500

    Refactored DOM driver

--- src/index.js
+++ src/index.js
@@ -8,7 +8,19 @@ function main(sources) {
         .startWith(null)
         .flatMapLatest(() =>
           Rx.Observable.timer(0, 1000)
-            .map(i => `seconds elapsed ${i}`)
+            .map(i => {
+              return {
+                tagName: 'H1',
+                children: [
+                  {
+                    tagName: 'span',
+                    children: [
+                      `Seconds elapsed: ${i}`
+                    ]
+                  }
+                ]
+              };
+            })
         ),
     Log: Rx.Observable.timer(0,2000)
         .map(i => 2*i),
@@ -20,12 +32,26 @@ function main(sources) {
 // sink: output (write) effects
 
 //imperative
-function DOMDriver(text) {
-  text.subscribe(text => {
-    const container = document.querySelector('#root');
-    container.textContent = text;
-  });
+function DOMDriver(objStream) {
+  function createElement(obj) {
+    const element = document.createElement(obj.tagName);
+    obj.children
+      .filter(c => typeof c === 'object')
+      .map(createElement)
+      .forEach(c => element.appendChild(c));
+    obj.children
+      .filter(c => typeof c === 'string')
+      .forEach(c => element.innerHTML += c);
+    // element.innerHTML = obj.children[0];
+    return element;
+  }
 
+  objStream.subscribe(obj => {
+    const container = document.querySelector('#root');
+    container.innerHTML = '';
+    const element = createElement(obj);
+    container.appendChild(element);
+  });
   // input from DOM (clicks from user)
   const DOMSource = Rx.Observable.fromEvent(document, 'click');
   return DOMSource;
