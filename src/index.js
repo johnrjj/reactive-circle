cec9f3c3902720cd1000936201cb740ae6d4e054
John J <johnrjj@gmail.com>
2016-01-31 22:15:46 -0500

    Getting my sink on

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import Rx from 'rx';
 
-Rx.Observable.timer(0, 1000)
-  .map(i => `Seconds elapsed ${i}`)
-  .subscribe(text => {
+//functional
+function main() {
+  return {
+    DOM:  Rx.Observable.timer(0, 1000)
+        .map(i => `Seconds elapsed ${i}`),
+    Log: Rx.Observable.timer(0,2000)
+        .map(i => (2*i)),
+  };
+}
+
+//imperative
+function DOMEffect(text) {
+  text.subscribe(text => {
     const container = document.querySelector('#root');
     container.textContent = text;
   });
+}
+
+//imperative
+function ConsoleLogEffect(text) {
+  text.subscribe(text => console.log(text));
+}
+
+
+//run
+const sinks = main();
+ConsoleLogEffect(sinks.Log);
+DOMEffect(sinks.DOM);
