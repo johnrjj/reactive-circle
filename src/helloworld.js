0a2f64a05af60a40c38ecb66afdfdd23497fc992
John J <johnrjj@gmail.com>
2016-02-02 14:33:35 -0500

    Hello world

---
+++ src/helloworld.js
@@ -0,0 +1,28 @@
+import Cycle from '@cycle/core';
+import {label, input, h1, hr, span, div, makeDOMDriver} from '@cycle/dom';
+
+function main(sources) {
+  //Restricts interest in input elements
+  //Returns an observable of input events happening on that field (input dom events)
+  const inputEv$ = sources.DOM.select('.fieldclass').events('input');
+  // Get each event, get target (actual dom element), get string inside element
+  const name$ = inputEv$.map(ev => ev.target.value).startWith(''); //needs a start with so name$ not empty
+  // every time the user makes a write effect, we create a new dom elem
+  const sinks = {
+    DOM: name$.map(name =>
+      div([
+        label('Name:'),
+        input('.fieldclass', {type: 'text'}),
+        hr(),
+        h1(`Hello ${name}!`),
+      ])
+    )
+  }
+  return sinks;
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#root'),
+}
+
+Cycle.run(main, drivers);
