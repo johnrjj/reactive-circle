faed617f69165be305eed9224234ee467b9e88ee
John J <johnrjj@gmail.com>
2016-02-04 13:49:32 -0500

    Multiple component instances

--- src/bmi-calculator-refactor.js
+++ src/bmi-calculator-refactor.js
@@ -1,5 +1,5 @@
 import Cycle from '@cycle/core';
-import {div, input, label, h2, makeDOMDriver} from '@cycle/dom';
+import { div, input, label, h2, makeDOMDriver } from '@cycle/dom';
 
 function intent(DOMSource) {
   return DOMSource.select('.slider').events('input')
@@ -30,25 +30,61 @@ function view(state$) {
   )
 }
 
-function main(sources) {
+function LabeledSlider(sources) {
   const change$ = intent(sources.DOM);
   const state$ = model(change$, sources.props);
   const vtree$ = view(state$);
-
   return {
     DOM: vtree$,
   };
 }
 
-const drivers = {
-  DOM: makeDOMDriver('#root'),
-  props: () => Rx.Observable.of({
+function main(sources) {
+  const weightProps$ = Rx.Observable.of({
+        label: 'Weight',
+        unit: 'kg',
+        min: 40,
+        max: 150,
+        init: 70,
+  });
+  const weightSinks =  LabeledSlider({
+    DOM: sources.DOM.select('.weight'), props: weightProps$
+  });
+  const weightVTree$ = weightSinks.DOM.map(vtree => {
+    vtree.properties.className += ' weight';
+    return vtree;
+  });
+
+  const heightProps$ = Rx.Observable.of({
         label: 'Height',
         unit: 'cm',
         min: 140,
         max: 220,
         init: 170,
-  })
+  });
+  const heightSinks =  LabeledSlider({
+    DOM: sources.DOM.select('.height'), props: heightProps$
+  });
+  const heightVTree$ = heightSinks.DOM.map(vtree => {
+    vtree.properties.className += ' height';
+    return vtree;
+  });
+
+  const vtree$ = Rx.Observable.combineLatest(
+    weightVTree$, heightVTree$, (weightVTree, heightVTree) =>
+      div([
+        weightVTree,
+        heightVTree,
+      ])
+  )
+  return {
+    DOM: vtree$
+  };
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#root'),
+
 }
 
 Cycle.run(main, drivers);
